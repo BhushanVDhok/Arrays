419d85462141a2b4c541b101864f2178d8670a56
Bhushan Dhok <dhokbhushan172@gmail.com>
2026-07-16 23:18:50 +0530

    Initial commit

---
+++ array.cpp
@@ -0,0 +1,19 @@
+// Create an array --> input n and take n integers into array --> print them.
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout << endl;
+    cout << "Array elements are: ";
+    for(int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
