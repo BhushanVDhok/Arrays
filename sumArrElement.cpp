b3c2f287aab9918c06f05b83959843e8f73e4998
Bhushan Dhok <dhokbhushan172@gmail.com>
2026-07-16 23:29:10 +0530

    commit first

---
+++ sumArrElement.cpp
@@ -0,0 +1,21 @@
+// Finding the sum of all elements in an array.
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout << endl;
+   
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum = sum + arr[i];
+    }
+     cout << "Sum of array elements is: "<< sum << endl; 
+    cout << endl;
+}
