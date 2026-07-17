dc605427e0e33644ef5606d36082f23beaa8537a
Bhushan Dhok <dhokbhushan172@gmail.com>
2026-07-17 17:44:39 +0530

    commit first

---
+++ maxElement.cpp
@@ -0,0 +1,22 @@
+// Finding max element from given array
+#include <bits/stdc++.h>
+using namespace std;
+int maxElement(int arr[], int n)
+{
+    int max = arr[0];
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+    }
+    return max;
+}
+int main()
+{
+    int arr[] = {-9, -2, -1, -11, -8};
+    int n = 5;
+    int max = maxElement(arr,n);
+    cout << "Maximum element from the given array is: " << max << endl;
+}
---
+++ minElement.cpp
@@ -0,0 +1,21 @@
+// Finding minimum element from given array
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout << endl;
+    int min = arr[0];
+    for(int i=1;i<n;i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }
+    }
+    cout << "Minimum element from the given array is: " << min << endl;
+}
