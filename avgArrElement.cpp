f530808d87da085c168f2c57637d74774fff842e
Bhushan Dhok <dhokbhushan172@gmail.com>
2026-07-17 16:53:59 +0530

    Initial commit

---
+++ avgArrElement.cpp
@@ -0,0 +1,23 @@
+// Finding average of all array elements 
+// avg = sum of all elements / total no of elements(n)
+#include <bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter array elements: ";
+    for(int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    cout << endl;
+   
+    int sum = 0;
+    for(int i=0;i<n;i++){
+        sum = sum + arr[i];
+    }
+    int avg = sum / n; 
+    cout << "avg is: " << avg << endl;
+    cout << endl;
+}
