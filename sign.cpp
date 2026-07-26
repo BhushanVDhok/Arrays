154eccb347a5a934db6c4692e6cbd02df7ab4e70
Bhushan Dhok <dhokbhushan172@gmail.com>
2026-07-26 22:43:28 +0530

    commit one

--- sign.cpp
+++ sign.cpp
@@ -2,37 +2,37 @@
 #include <bits/stdc++.h>
 using namespace std;
 // Bruteforce approach
-// vector<int> rearrangeArray(vector<int> &arr, int n){
-//     // vector to store positive array
-//     vector<int> pos;
-//     // vector to store negative array
-//     vector<int> neg;
-//     // Stores elements in pos & neg array a/c to sign
-//     for(int i=0;i<n;i++){
-//         if(arr[i] > 0){
-//             pos.push_back(arr[i]);
-//         }
-//         else{
-//             neg.push_back(arr[i]);
-//         }
-//     }
-//     // Place +ve at even indices and -ve at odd indices
-//     for(int i=0;i<(n/2);i++){
-//         arr[2 * i] = pos[i];
-//         arr[2* i + 1] = neg[i];
-//     }
-//     return arr;
-// }
-// int main(){
-//     vector<int> arr = {3,1,-2,-5,2,-4};
-//     int n = arr.size();
-//     vector<int> result = rearrangeArray(arr,n);
-//     for(int i=0;i<n;i++){
-//         cout << result[i] << " ";
-//     }
-//     cout << endl;
-//     return 0;
-// }
+vector<int> rearrangeArray(vector<int> &arr, int n){
+    // vector to store positive array
+    vector<int> pos;
+    // vector to store negative array
+    vector<int> neg;
+    // Stores elements in pos & neg array a/c to sign
+    for(int i=0;i<n;i++){
+        if(arr[i] > 0){
+            pos.push_back(arr[i]);
+        }
+        else{
+            neg.push_back(arr[i]);
+        }
+    }
+    // Place +ve at even indices and -ve at odd indices
+    for(int i=0;i<(n/2);i++){
+        arr[2 * i] = pos[i];
+        arr[2* i + 1] = neg[i];
+    }
+    return arr;
+}
+int main(){
+    vector<int> arr = {3,1,-2,-5,2,-4};
+    int n = arr.size();
+    vector<int> result = rearrangeArray(arr,n);
+    for(int i=0;i<n;i++){
+        cout << result[i] << " ";
+    }
+    cout << endl;
+    return 0;
+}
 
 // ----------------------------------------------------------------X-----------------------------------------------------------------
 // Optimal approach
